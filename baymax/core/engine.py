bb6f43f447baa14e4864faa7daa65ac1fdd7ce43
vijay yellepeddi <vyellepeddi@castlighthealth.com>
2014-12-12 18:16:10 +0530

    doing some exp

--- baymax/core/engine.py
+++ baymax/core/engine.py
@@ -14,23 +14,20 @@ class Engine(object):
 			'gantt':GanttChartJSGenerator
 		}
 
-	def generate(self,arg):
-		plot_type = arg.split()[0]
-		params = arg.split()[1]
-
+	def generate(self,plot_type,params):
 		if not plot_type in self.plot_types.keys():
 			return "alert('PLOT UNSUPPORTED');"
 
 		return self.plot_types[plot_type]().make(params)
 
 
-	def render(self,filepath):
+	def render(self,filepath,params={}):
 		response = ""
 		with open(filepath,'r') as file:
 			for line in file:
 				try:
-					cmd = re.search(r'{# (.*?) #}',line).groups()[0]
-					js = self.generate(cmd)
+					plot_type = re.search(r'{# (.*?) #}',line).groups()[0]
+					js = self.generate(plot_type,params)
 					response += re.sub(r'({#.*?#})',js,line)
 				except AttributeError:			
 					response += line
